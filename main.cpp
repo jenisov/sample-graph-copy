aaa59cfb58c246ec2c76a2a6f1ab2d0a4933327a
Andrey Gostev <Gostev71@outlook.com>
2022-05-30 18:28:33 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <cassert>
 #include <unordered_map>
 #include <random>
-#include <string>
 
 #define LIST_SIZE 10
 
@@ -21,11 +20,8 @@ private:
     friend class list<T>;
     friend class graph;
 
-    using this_ptr = list_node<T>*;
-    using data_type = T;
-
-    this_ptr m_next;
-    data_type m_data;
+    list_node<T> *m_next;
+    T m_data;
 
 public:
     list_node() : m_data(), m_next(nullptr) {}
@@ -38,41 +34,32 @@ class list {
 public:
     class iterator;
 protected:
-    using iter_type = list<T>::iterator;
-    using node_type = list_node<T>;
-    using node_ptr = list_node<T>*;
-    using data_ref = T&;
-
     size_t m_size;
 
-    node_ptr m_head;
-    node_ptr m_end;
+    list_node<T> *m_head;
+    list_node<T> *m_end;
 
 public:
     class iterator {
     private:
-        using this_type = list<T>::iterator;
-        using this_ref = list<T>::iterator&;
-        using ptr_type = node_ptr;
-
-        ptr_type m_ptr;
+        list_node<T> *m_ptr;
 
     public:
         iterator() : m_ptr(nullptr) {}
 
-        explicit iterator(const ptr_type& ptr) : m_ptr(ptr) {}
+        explicit iterator(list_node<T> *ptr) : m_ptr(ptr) {}
 
         ~iterator() = default;
 
-        data_ref operator*() const {
+        T& operator*() const {
             return this->m_ptr->m_data;
         }
 
-        const ptr_type operator->() const {
+        const list_node<T>* operator->() const {
             return this->m_ptr;
         }
 
-        const this_ref operator++() {
+        const list<T>::iterator& operator++() {
             if (!this->m_ptr) {
                 throw std::exception("Iterator not incrementable.");
             }
@@ -80,18 +67,18 @@ public:
             return *this;
         }
 
-        const this_type operator++(int) {
-            this_type old;
+        const list<T>::iterator operator++(int) {
+            list<T>::iterator old;
             old.m_ptr = this->m_ptr;
             this->m_ptr = this->m_ptr->m_next;
             return old;
         }
 
-        bool operator==(const this_ref right) {
+        bool operator==(const list<T>::iterator &right) {
             return this->m_ptr == right.m_ptr;
         }
 
-        bool operator!=(const this_ref right) {
+        bool operator!=(const list<T>::iterator &right) {
             return this->m_ptr != right.m_ptr;
         }
 
@@ -108,21 +95,21 @@ public:
 
     void push_back(const T& arg) {
         if (!this->m_head) {
-            this->m_head = new node_type;
+            this->m_head = new list_node<T>;
             this->m_head->m_data = arg;
             ++this->m_size;
             this->m_end = this->m_head;
             return;
         }
-        this->m_end->m_next = new node_type;
+        this->m_end->m_next = new list_node<T>;
         this->m_end->m_next->m_data = arg;
         this->m_end = this->m_end->m_next;
         ++this->m_size;
     }
 
-    data_ref operator[] (const int& pos) const {
+    T& operator[] (const int& pos) const {
         assert(pos > -1 && pos < this->m_size);
-        node_ptr ptr = this->m_head;
+        list_node<T> *ptr = this->m_head;
         for (int i = 0; i < pos; i++) {
             ptr = ptr->m_next;
         }
@@ -134,8 +121,8 @@ public:
         if (!this->m_head) {
             return;
         }
-        node_ptr ptr = this->m_head;
-        node_ptr next;
+        list_node<T> *ptr = this->m_head;
+        list_node<T> *next;
         while (ptr) {
             next = ptr->m_next;
             delete[] ptr;
@@ -145,20 +132,20 @@ public:
         this->m_size = 0;
     }
 
-    iter_type begin() {
+    list<T>::iterator begin() {
         return list<T>::iterator(this->m_head);
     }
 
-    iter_type end() {
+    list<T>::iterator end() {
         return list<T>::iterator(nullptr);
     }
 
-    iter_type pre_end() {
+    list<T>::iterator pre_end() {
         return list<T>::iterator(this->m_end);
     }
 
     friend std::ostream& operator<< (std::ostream& out, const list<T>& l) {
-        node_ptr current = l.m_head;
+        list_node<T> *current = l.m_head;
         for (current; current; current = current->m_next) {
             out << current->m_data << " ";
         }
@@ -196,22 +183,22 @@ public:
         this->push_back(0);
         vec.emplace(right.m_head, this->m_head);
         for (auto ptr = right.m_head; ptr; ptr = ptr->m_next) {
-            node_ptr data = nullptr;
-            node_ptr next = nullptr;
+            list_node<T> *data = nullptr;
+            list_node<T> *next = nullptr;
             if (ptr->m_data) {
                 if (vec.find(ptr->m_data) == vec.end()) {
-                    data = new node_type;
+                    data = new list_node<T>;
                     vec.emplace(ptr->m_data, data);
                 } else {
-                    data = (node_ptr)vec[ptr->m_data];
+                    data = (list_node<T> *)vec[ptr->m_data];
                 }
             }
             if (ptr->m_next) {
                 if (vec.find(ptr->m_next) == vec.end()) {
-                    next = new node_type;
+                    next = new list_node<T>;
                     vec.emplace(ptr->m_next, next);
                 } else {
-                    next = (node_ptr)vec[ptr->m_next];
+                    next = (list_node<T> *)vec[ptr->m_next];
                 }
             }
             this->m_end->m_next = next;
@@ -248,7 +235,7 @@ int main()
     void* vec[LIST_SIZE];
     for (auto &i : vec) {
         lst.push_back(0);
-        i = lst.pre_end().operator->();
+        i = (void*)lst.pre_end().operator->();
     }
 
     for (auto &el : lst) {
