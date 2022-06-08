bf443f3e0e1dfddeb918350458cff56a052ac75a
Andrey Gostev <Gostev71@outlook.com>
2022-06-08 17:10:17 +0300

    create recursive copy

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cassert>
 #include <unordered_map>
 #include <random>
 
@@ -8,6 +7,8 @@
 using std::unordered_map;
 using std::cout;
 using std::mt19937;
+using std::exception;
+using std::array;
 
 class graph;
 
@@ -23,67 +24,75 @@ private:
 public:
     graph_node() : m_next(nullptr), m_link(nullptr), m_data(0) {}
 
-    ~graph_node() = default;
+    graph_node(const graph_node& right) : m_next(nullptr), m_link(right.m_link), m_data(right.m_data) {}
 
-    void bind(graph_node *link) {
+    ~graph_node() {
+        this->m_link = nullptr;
+        this->m_next = nullptr;
+        this->m_data = 0;
+    }
+
+    void bind(graph_node* link) {
         this->m_link = link;
     }
 };
 
 class graph {
-public:
-    class iterator;
 private:
     size_t m_size;
 
-    graph_node *m_head;
-    graph_node *m_end;
+    graph_node* m_head;
+    graph_node* m_end;
 
 public:
     class iterator {
     private:
-        graph_node *m_ptr;
+        graph_node* m_ptr;
 
     public:
         iterator() : m_ptr(nullptr) {}
 
-        explicit iterator(graph_node *ptr) : m_ptr(ptr) {}
+        explicit iterator(graph_node* ptr) : m_ptr(ptr) {}
+
+        iterator(const graph::iterator& right) = default;
 
         ~iterator() = default;
 
         size_t& operator*() const {
+            if (!this->m_ptr) {
+                throw exception("Cannot dereference end graph iterator.");
+            }
             return this->m_ptr->m_data;
         }
 
-        graph_node *operator->() const {
+        graph_node* operator->() const {
             return this->m_ptr;
         }
 
         const graph::iterator& operator++() {
             if (!this->m_ptr) {
-                throw std::exception("Iterator not incrementable.");
+                throw exception("Cannot increment end graph iterator.");
             }
             this->m_ptr = this->m_ptr->m_next;
             return *this;
         }
 
         const graph::iterator operator++(int) {
-            graph::iterator old;
-            old.m_ptr = this->m_ptr;
-            this->m_ptr = this->m_ptr->m_next;
+            graph::iterator old(*this);
+            ++*this;
             return old;
         }
 
-        bool operator==(const graph::iterator &right) {
+        bool operator==(const graph::iterator& right) const {
             return this->m_ptr == right.m_ptr;
         }
 
-        bool operator!=(const graph::iterator &right) {
+        bool operator!=(const graph::iterator& right) const {
             return this->m_ptr != right.m_ptr;
         }
 
-        explicit operator bool() {
-            return (bool)this->m_ptr;
+        explicit operator bool() const {
+            return (bool) this->m_ptr;
         }
     };
 
@@ -106,40 +115,6 @@ public:
         }
     }
 
-    // более медленное копирование за один проход
-/*    graph(const graph &right) {
-        std::unordered_map<void*, void*> vec;
-        this->clear();
-        this->push_back(0);
-        vec.emplace(right.m_head, this->m_head);
-        for (auto ptr = right.m_head; ptr; ptr = ptr->m_next) {
-            list_node<T> *data = nullptr;
-            list_node<T> *next = nullptr;
-            if (ptr->m_data) {
-                if (vec.find(ptr->m_data) == vec.end()) {
-                    data = new list_node<T>;
-                    vec.emplace(ptr->m_data, data);
-                } else {
-                    data = (list_node<T> *)vec[ptr->m_data];
-                }
-            }
-            if (ptr->m_next) {
-                if (vec.find(ptr->m_next) == vec.end()) {
-                    next = new list_node<T>;
-                    vec.emplace(ptr->m_next, next);
-                } else {
-                    next = (list_node<T> *)vec[ptr->m_next];
-                }
-            }
-            this->m_end->m_next = next;
-            this->m_end->m_data = data;
-            if (next) {
-                this->m_end = next;
-                this->m_size++;
-            }
-        }
-    }*/
-
     ~graph() {
         this->clear();
     }
@@ -150,11 +125,11 @@ public:
             this->m_size = 0;
             return;
         }
-        graph_node *ptr = this->m_head;
-        graph_node *next;
+        graph_node* ptr = this->m_head;
+        graph_node* next;
         while (ptr) {
             next = ptr->m_next;
-            delete[] ptr;
+            delete ptr;
             ptr = next;
         }
         this->m_head = nullptr;
@@ -162,7 +137,7 @@ public:
         this->m_size = 0;
     }
 
-    void push_back(const size_t& val, graph_node *link = nullptr) {
+    void push_back(const size_t& val, graph_node* link = nullptr) {
         if (!this->m_head) {
             this->m_head = new graph_node;
             this->m_head->m_data = val;
@@ -178,19 +153,25 @@ public:
         ++this->m_size;
     }
 
-    graph::iterator begin() {
+    graph::iterator begin() const {
         return graph::iterator(this->m_head);
     }
 
-    graph::iterator end() {
+    graph::iterator end() const {
         return graph::iterator(nullptr);
     }
 
-    graph::iterator pre_end() {
+    graph::iterator pre_end() const {
         return graph::iterator(this->m_end);
     }
 
-    void print() {
+    // функция, подготавливающая граф к рекурсивному копированию и вызывающая функцию копирования
+    void copy(graph::iterator head) {
+        this->clear();
+        this->recursive_copy(head);
+    }
+
+    void print() const {
         unordered_map<void*, int> vec;
         int i = 1;
         for (auto ptr = this->m_head; ptr; ptr = ptr->m_next) {
@@ -198,31 +179,56 @@ public:
         }
         i = 1;
         for (auto ptr = this->m_head; ptr; ptr = ptr->m_next) {
-            std::cout << i++ << " -> " << vec[ptr->m_link] << "\n";
+            std::cout << i++ << " (" << ptr->m_data << ") -> " << vec[ptr->m_link] << "\n";
             if (ptr->m_next) {
                 cout << "|\n";
             }
         }
+    }
+
+private:
+    void recursive_copy(graph::iterator head) {
+        // инициализируем вершину графа
+        static auto this_ptr = this->m_head = new graph_node(*head.operator->());
+        auto current_ptr = this_ptr;
+        static unordered_map<graph_node*, graph_node*> vec;
+        // добавляем ассоциацию узлов одного графа узлу другого графа
+        vec.emplace(head.operator->(), this_ptr);
+        if (head->m_next) {
+            ++head;
+            // инициализация узла
+            this_ptr->m_next = new graph_node(*head.operator->());
+            this_ptr = this_ptr->m_next;
+            this->recursive_copy(head);
+        }
+        // переопределяем линк узла графа
+        current_ptr->m_link = vec[current_ptr->m_link];
     }
 };
 
 int main() {
     graph lst;
     mt19937 rg;
+    // массив, содержащий адресы узлов графа
     graph_node* vec[LIST_SIZE];
-    for (auto &i : vec) {
-        lst.push_back(0);
+    // инициализируем граф
+    for (auto& i: vec) {
+        lst.push_back(rg() % 100 + 1);
         i = lst.pre_end().operator->();
     }
 
+    // связываем узлы графа со случайными узлами этого графа
     for (auto it = lst.begin(); it != lst.end(); it++) {
         it->bind(vec[rg() % LIST_SIZE]);
     }
+    cout << "\n";
 
-    graph lst1(lst);
+    graph lst1;
+    lst1.copy(lst.begin());
 
     lst.print();
-    std::cout << "\n\n";
+    cout << "\n\n";
     lst1.print();
+    lst.clear();
     return 0;
 }
